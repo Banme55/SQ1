f6108ef5e01437c3ba9d83db391798ed8297f124
Banme55 <80745583+Banme55@users.noreply.github.com>
2021-03-17 20:10:30 +0800

    Update iboxpayCOOKIE.js

--- Task/iboxpayCOOKIE.js
+++ Task/iboxpayCOOKIE.js
@@ -2,12 +2,7 @@
 
 //独立COOKIE文件     ck在``里面填写，多账号换行
 
-let refreshtokenVal= `26f08690c1df41038d0dd34a066b611a
-66e8a01d45344d729b23e16b9a9e1ddd
-a8ba9d96f32944da913fddf037edaaf5
-8e29f167f1a543a0ad0bd94b1822ad1f
-b7093a0fee914fb4891d8c1d4eb8df3b
-51c172c1c8de4507afa7e6e6dac68e61`
+let refreshtokenVal= ``
 
 
 let iboxpaycookie = {
